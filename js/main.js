8604681f9d37f3c117186e3ccb6d79aac47e56b9
edwin felipe <edwinisfelipe25@gmail.com>
2018-08-26 15:56:43 -0400

    ya el personaje y los coins son redondos y se agrego el audio al comer un coin

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ dir = null,
 wall = [],
 coin =[],
 pacman;
+var comiendo = new Audio();
 
 //objetos que nos permitiran crear los obstaculos y personajes del juego
 var square = function(x,y,wi,he,color){
@@ -20,11 +21,24 @@ var circle = function(x,y,r,color){
     this.x = x;
     this.y = y;
     this.r = r;
+    
     this.color = color;
     
 };
 // en los siguientes prototipos se agregan a los objetos funciones que los dibujan y los mueven por la pantalla
 square.prototype ={
+    get left(){
+        return this.x;
+    },
+    get right(){
+        return this.x + this.wi;
+    },
+    get top(){
+        return this.y;
+    },
+    get bottom(){
+        return this.y + this.he;
+    },
     draw: function(){
         cx.beginPath();
         cx.fillStyle = this.color;
@@ -32,8 +46,31 @@ square.prototype ={
         cx.fill();
         cx.closePath();
 
+    }
+};
+circle.prototype ={
+    get left(){
+        return this.x - this.r;
     },
-    move: function(TheKey){
+    get right(){
+        return this.x + this.r;
+    },
+    get top(){
+        return this.y - this.r;
+    },
+    get bottom(){
+        return this.y + this.r;
+    },
+
+
+    draw: function(){
+        cx.beginPath();
+        cx.fillStyle = this.color;
+        cx.moveTo(this.x,this.y);
+        cx.arc(this.x,this.y,this.r,Math.PI*2,0,true);
+        cx.fill();
+        cx.closePath();
+    },move: function(TheKey){
         this.lastX = this.x;
         this.lastY = this.y;
         if(TheKey===37){
@@ -62,21 +99,10 @@ square.prototype ={
         }       
     },
     intersects: function(rect){
-        return (this.x < rect.x + rect.wi &&
-            this.x + this.wi > rect.x &&
-            this.y < rect.y + rect.he &&
-            this.y + this.he > rect.y);
-    }
-    
-};
-circle.prototype ={
-    draw: function(){
-        cx.beginPath();
-        cx.fillStyle = this.color;
-        cx.moveTo(this.x,this.y);
-        cx.arc(this.x,this.y,this.r,Math.PI*2,0,true);
-        cx.fill();
-        cx.closePath();
+        return (this.left < rect.right &&
+           this.right > rect.left &&
+            this.top < rect.bottom &&
+            this.bottom > rect.top);
     }
 };
 //es el tile map de donde se dibujara el escenario
@@ -107,9 +133,10 @@ var level0=[
 function init(){
     cv = document.getElementById("lienzo");
     cx = cv.getContext("2d");
-    pacman = new square(90,90,30,30,"#FFFF00");
+    comiendo.src = "../media/come.mp3"
+    pacman = new circle(105,105,15,"#FFFF00");
     setMap(level0, 30);
-    window.setInterval(run,90);
+    window.setInterval(run,150);
 }
 //esta funcion almacena los objetos en array para luego dibujarlos
 function setMap(map, blockSize){
@@ -123,8 +150,7 @@ function setMap(map, blockSize){
                 wall.push(new square(col * blockSize, ind * blockSize, blockSize, blockSize, "#0055ff"));
             }
             if(map[ind][col]=== 0){
-               // coin.push(new circle((col*blockSize) + (blockSize / 2),(ind*blockSize) +(blockSize / 2),5,"gold"));
-               coin.push(new square(col * blockSize, ind * blockSize, 10, 10, "gold"))
+               coin.push(new circle((col*blockSize) + (blockSize / 2),(ind*blockSize) +(blockSize / 2),5,"gold"));
             }
         }
     }
@@ -153,7 +179,8 @@ function actions(){
     for(i = 0; i < coin.length; i++){
         if(pacman.intersects(coin[i])){
           
-            coin.splice(coin.indexOf(coin[i]), 1)
+            coin.splice(coin.indexOf(coin[i]), 1);
+            comiendo.play();
          }
     }
     
@@ -169,4 +196,5 @@ window.addEventListener("keydown",function(evt){
     lastPress = code;
 });
 init();
+
 };
