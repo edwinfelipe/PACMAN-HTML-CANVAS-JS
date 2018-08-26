7c0cbc599c9c4aa35b3fc783e44a6ef2c47db661
edwin felipe <edwinisfelipe25@gmail.com>
2018-08-26 14:00:21 -0400

    primera base, el codigo fue obtimizado y ya el juego corre sin nigun tipo de bug

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 window.onload = function(){
-var cv = document.getElementById("lienzo");
-var cx = cv.getContext("2d");
-var lastPress = null;
-var dir = null;
- 
+//declaracion de la varibles
+var cv,
+cx,
+lastPress = null,
+dir = null,
+wall = [],
+coin =[],
+pacman;
+
 var square = function(x,y,wi,he,color){
     this.x = x;
     this.y = y;
     this.wi = wi;
     this.he = he;
     this.color = color;
-    
 };
 var circle = function(x,y,r,color){
     this.x = x;
@@ -19,18 +22,6 @@ var circle = function(x,y,r,color){
     this.color = color;
     
 };
-var player = function(x,y,wi,he,color,dir){
-    this.x = x;
-    this.y = y;
-    this.wi = wi;
-    this.he = he;
-    this.color = color;
-    this.dir = dir;
-
-   
-}
-
-
 square.prototype ={
     draw: function(){
         cx.beginPath();
@@ -39,18 +30,10 @@ square.prototype ={
         cx.fill();
         cx.closePath();
 
-    }
-    
-};
-
-player.prototype ={
-    draw: function(){
-        cx.beginPath();
-        cx.fillStyle = this.color;
-        cx.fillRect(this.x,this.y,this.wi,this.he);
-        cx.closePath();
     },
     move: function(TheKey){
+        this.lastX = this.x;
+        this.lastY = this.y;
         if(TheKey===37){
             dir = 0;
         }
@@ -63,27 +46,18 @@ player.prototype ={
         else if(TheKey === 40){
             dir = 3;
         }
-
-        this.lastX = this.x;
-        this.lastY = this.y;
         if(dir === 0 ){
         this.x -= 30;
-        
         }
-        if(dir === 1 ){
-            this.y -= 30;
-            
+        else if(dir === 1 ){
+            this.y -= 30;   
         }
-        if(dir === 2 ){
-            this.x += 30;
-            
+        else if(dir === 2 ){
+            this.x += 30;   
         }
-        if(dir === 3 ){
-            this.y += 30;
-            
-        }
-            
-        
+        else if(dir === 3 ){
+            this.y += 30;   
+        }       
     },
     intersects: function(rect){
         return (this.x < rect.x + rect.wi &&
@@ -91,6 +65,7 @@ player.prototype ={
             this.y < rect.y + rect.he &&
             this.y + this.he > rect.y);
     }
+    
 };
 circle.prototype ={
     draw: function(){
@@ -102,10 +77,6 @@ circle.prototype ={
         cx.closePath();
     }
 };
-//elementos
-var wall = [];
-var coin =[];
-var pacman = new player(90,90,30,30,"#FFFF00",dir);
 var level0=[
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     [1,3,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,3,1],
@@ -129,9 +100,15 @@ var level0=[
     [1,3,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,3,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ];
-
+function init(){
+    cv = document.getElementById("lienzo");
+    cx = cv.getContext("2d");
+    pacman = new square(90,90,30,30,"#FFFF00");
+    setMap(level0, 30);
+    window.setInterval(run,90);
+}
 function setMap(map, blockSize){
-    var col = 0,  
+    let col = 0,  
     columns = 0;
     wall.length = 0;
     for(row of map){
@@ -148,9 +125,18 @@ function setMap(map, blockSize){
     }
 }
 function draw(){
-    cx.clearRect(0,0,cv.clientWidth,cv.height);
+    cx.clearRect(0,0,cv.width,cv.height);
     for(let w of wall){
         w.draw();
+    }
+    for(i = 0; i < coin.length; i++){
+        coin[i].draw();
+    }
+    pacman.draw();
+}
+function actions(){
+    pacman.move(lastPress);
+    for(let w of wall){
         if(pacman.intersects(w)){
             pacman.x = pacman.lastX;
             pacman.y = pacman.lastY;
@@ -158,27 +144,21 @@ function draw(){
         }
     }
     for(i = 0; i < coin.length; i++){
-        coin[i].draw();
-
         if(pacman.intersects(coin[i])){
           
-           coin.splice(coin.indexOf(coin[i]), 1)
-        }
+            coin.splice(coin.indexOf(coin[i]), 1)
+         }
     }
-    pacman.draw();
+    
+    
 }
-setMap(level0, 30);
 function run(){
-    
-    pacman.move(lastPress);
-    
+    actions();
     draw();
-    
 }
-
-window.setInterval(run,90);
 window.addEventListener("keydown",function(evt){
     let code = evt.keyCode; 
     lastPress = code;
 });
+init();
 };
